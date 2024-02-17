4e8e573db9dd578481b0b5448bdf58412f248f46
ParkMini <parkteammini@gmail.com>
2024-02-17 17:18:37 +0900

    Initial Commit

---
+++ src/main/java/highton/team2/NonameApplication.java
@@ -0,0 +1,13 @@
+package highton.team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/highton/team2/NonameApplicationTests.java
@@ -0,0 +1,13 @@
+package highton.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
