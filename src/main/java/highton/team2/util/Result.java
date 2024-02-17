9cd0a241927670a743edd7444a2368e62f4160b8
ParkMini <parkteammini@gmail.com>
2024-02-17 20:11:58 +0900

    Result에서 (String) message -> Generic(T) message 으로 수정

--- src/main/java/highton/team2/util/Result.java
+++ src/main/java/highton/team2/util/Result.java
@@ -5,17 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @Getter
-public class Result {
+public class Result<T> {
     private final int status;
-    private final String data;
+    private final T data;
 
-    public Result(int status, String data) {
+    public Result(int status, T data) {
         this.status = status;
         this.data = data;
     }
 
-    public static ResponseEntity<Result> of(HttpStatus httpStatus, String message) {
-        Result result = new Result(httpStatus.value(), message);
+    public static <T> ResponseEntity<Result<T>> of(HttpStatus httpStatus, T message) {
+        Result<T> result = new Result<>(httpStatus.value(), message);
         return new ResponseEntity<>(result, httpStatus);
     }
 }
